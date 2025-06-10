991e6d91627ff64c6c4c084c8f2fd1a2d56db5ff
tranthanhtung tranthanhtung <ttt28112000@gmail.com>
2025-06-10 22:57:02 +0700

    create ui destination

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Homepage from "./pages/HomePage/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
 import HotelDetail from "./pages/HotelDetail/HotelDetail";
 import MyBookings from "./pages/MyBookings/MyBookings";
 import HotelRooms from "./pages/HotelRooms/HotelRooms";
-import LoginPage from "./pages/Auth/LoginPage";
+import Login from "./pages/Auth/LoginPage";
 import Payment from "./pages/Payment/Payment";
+import About from "./pages/About/About";
+import Destination from "./pages/Destination/Destination";
 import "./App.scss";
 
 function App() {
@@ -16,12 +18,14 @@ function App() {
         <Header />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<Homepage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/hotels" element={<HotelRooms />} />
             <Route path="/hotel/:id" element={<HotelDetail />} />
             <Route path="/bookings" element={<MyBookings />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/payment" element={<Payment />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/destination" element={<Destination />} />
           </Routes>
         </main>
         <Footer />
