1b5cdd471aad43d8a1daa7ff7cf1806041e563e0
NHMHieuA1K57 <124542355+NHMHieuA1K57@users.noreply.github.com>
2025-06-11 20:34:49 +0700

    del

--- FE/src/components/Admin/Navbar.jsx
+++ FE/src/components/Admin/Navbar.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-200 py-3 bg-white transition-all duration-300'>
+        <link to='/' className='text-2xl font-bold text-gray-800'>QuickStay</link>
       
     </div>
   )
