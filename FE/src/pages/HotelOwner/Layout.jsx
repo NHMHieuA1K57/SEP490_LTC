5ca986dbc509c9e885b45638edac11cade42cc0b
NHMHieuA1K57 <124542355+NHMHieuA1K57@users.noreply.github.com>
2025-07-28 15:06:58 +0700

    delenav

--- FE/src/pages/HotelOwner/Layout.jsx
+++ FE/src/pages/HotelOwner/Layout.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Layout.css'
-import Navbar from '../../components/HotelOwner/Navbar'
 import Sidebar from '../../components/HotelOwner/Sidebar'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <div className='layout-container'>
-        <Navbar />
         <div className='layout-content'>
             <Sidebar />
             <div className='layout-outlet'>
